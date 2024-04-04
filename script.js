e15841e2f7a66246fdfc0a632153463f0ea8de80
TylerErnst <tcernst5@gmail.com>
2024-04-04 14:48:15 -0600

    Part 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ***** Part 1: Stack Overflow *****
+
 // Declare a global counter variable.
 let stackCount = 0;
 // Create a simple function that increments the variable, and then calls itself recursively.
@@ -16,13 +18,36 @@ try {
 }
 
 
+// ***** Part 2: Trampolines *****
+
+// Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
+
+  function flatten(arr) {
+    var flatArr = [];
+    for(var i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            flatArr = flatArr.concat(flatten(arr[i]));
+        } else {
+            flatArr.push(arr[i]);
+        }
+    }
+    return flatArr;
+}
+// Once your recursive function is complete, trampoline it.
+//*
+//*
+//*
+
+
+
+// ***** Part 3: Deferred Execution *****
 
 // Create a simple HTML element to hold text. Cache this HTML element into a JavaScript variable.
 const textElement = document.getElementById('text');
 // Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
 // Once complete, use the alert() method to alert the user that the calculation is finished.
 
-let n = 1000000;
+let n = 100;
 
 function addPrimes(n,element){
     let num = 0;
@@ -38,9 +63,9 @@ function addPrimes(n,element){
 }
 
 addPrimes(n,textElement)
-setTimeout(() => {
-    alert("Calculation Done");
-},5)
+// setTimeout(() => {
+//     alert("Calculation Done");
+// },5)
 
 
 
